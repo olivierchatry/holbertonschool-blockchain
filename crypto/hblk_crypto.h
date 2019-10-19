3dc1097e68bd5fdfbf4080d62d8445df9cae7af7
Olivier Chatry <olivier@futureon.com>
2019-10-19 12:36:41 +0200

    initial commit

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -6,8 +6,11 @@
 
 // taking from openssl crypto/sha/sha.h
 
+#define UNUSED(x) UNUSED_ ## x __attribute__((unused))
+
 #define SHA256_DIGEST_LENGTH 32
 
 uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]);
+
 
 #endif
